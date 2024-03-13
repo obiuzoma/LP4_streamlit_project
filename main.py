b683a0aa481b1a8f60cc4f2a2198c6302acffa39
Obiefuna Uzoma <uzomaezife@gmail.com>
2024-03-13 13:18:59 +0100

    read csv file into the data page and connect prediction page to history page

--- main.py
+++ main.py
@@ -10,38 +10,48 @@ st.set_page_config(
     layout='wide'
 )
 st.title('Home Page',"🏛️home:")
-st.image('time_series_image.jpg')
+# st.image('time_series_image.jpg')
 
 def project_description():
     col1, col2 = st.columns(2)
     with col1:
 
         st.markdown('#### Customer Churn Prediction')
-        st.write('customer churn prediction app is designed to help managers predict the likelyhood of customers leaving their company or not')
-        st.subheader('Key Features')
-        st.markdown('##### * Home page')
-        st.write('Login details of the app')
-        st.markdown('##### * Data page')
-        st.write('Sample of data use for the traing of the models')
-        st.markdown('##### * Dashboard')
-        st.write('This page contain the KPI and EDA of the customer data')
-        st.markdown('##### * Prediction Page')
-        st.write('A form to predict wether a customer will stop using company product')
-        st.markdown('##### * History Page')
-        st.write('Table containing the data of predicted cuntomers')
-    with col2:
-        st.subheader('How to run the app')
-        st.write('clone this repositary on your local machine')
-        st.write('cd my_folder')
-        st.write('clone https://github.com/obiuzoma/LP4_streamlit_project.git')
-        st.write('Change into the cloned repository')
-        st.write('cd LP4_streamlit_project')
-        st.write('Create a virtual environment')
-        st.write('python -m virtual_env env')
-        st.write('Activate the virtual environment')
+        ('''
+         
+        customer churn prediction app is designed to help managers predict the likelyhood of customers leaving their company or not
 
-        st.markdown('##### * Need Help?')
-        st.write('Congtact @: uzyivy@gmail.com')
+        **Key Features**
+        - Home page: Login details and app descriptions
+        - Data page:Sample of data use for the traing of the models
+        - Dashboard: contain the KPI and EDA of the customer data interactive visualization and insights
+        -  Prediction Page:A form to predict wether a customer will stop using company product
+        - History Page:Table containing the data of predicted cuntomers
+    ''')
+    with col2:
+        pass
+        ('''
+        **How to run the app**
+        - clone this repositary on your local machine
+        
+        cd my_folder
+        clone https://github.com/obiuzoma/LP4_streamlit_project.git
+         
+        
+        - Change into the cloned repository
+        cd LP4_streamlit_project
+        - Create a virtual environment:
+
+        python -m virtual_env env
+        - Activate the virtual environment:
+        virtual_env/scripts/activate
+
+        **Need Help?**
+
+        Congtact @: uzyivy@gmail.com
+        ''')
+
+
 st.write('login Details')
 st.write('User Name: uzoma')
 st.write('Passward: abc')
